d0e8c6cdaeffc088d9975c4a688c63c2a15c7ed5
Pranavgulati <pranavgulati95@yahoo.com>
2016-10-07 20:17:06 +0530

    release 1

--- CM_SS_client/CM_SS_client.ino
+++ CM_SS_client/CM_SS_client.ino
@@ -7,16 +7,18 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
+#include <EEPROM.h>
 #define DEBUG 1
 #define NUM_MAX_CLIENT 4 
 #define CLIENT_PORT 4444	  // port to send UDP packets to clients
 #define SERVER_PORT 8888      // local port to listen for UDP packets
 #define SEPARATORbeg "."
-#define SEPARATORend "."
-#define MYNAME ".channel1." 
+#define SEPARATORend ">"
+#define MYNAME ".channel1>" 
+
 /*
 format for chat string
-<name>message
+.name>message
 */
 const char* serverSSID = "myServer";			//  your network SSID (name)
 const char* password = "passphrase";       // your network password
@@ -24,13 +26,14 @@ const char* password = "passphrase";       // your network password
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
 IPAddress serverIP;
-
+unsigned long int TIME=0;
+byte unconnected=1;
 
 void printWifiStatus() {
 	// print the SSID of the network you're attached to:
 	// print your WiFi shield's IP address:
 	IPAddress ip = WiFi.localIP();
-	Serial.print("IP Address: ");
+	Serial.print("\n\nIP Address: ");
 	Serial.println(ip);
 	Serial.print("gateway IP Address: ");
 	Serial.println(WiFi.gatewayIP());
@@ -42,12 +45,32 @@ void sendMessage(String data, IPAddress sendIP) {
 	Udp.endPacket();
 }
 
-
+#define SIGN 40
+void debuggerInit(){
+  
+  //error checking and self diagnosis code do not alter
+  EEPROM.begin(64);
+  if (EEPROM.read(SIGN) != 0x22){
+    EEPROM.write(SIGN, 0x22);
+    EEPROM.write(SIGN + 1, 40);
+    EEPROM.commit();
+    EEPROM.end();
+  }
+  else{
+    byte status = 0;
+    status = EEPROM.read(SIGN + 1);
+    EEPROM.write(SIGN + 1, status - 1);
+    EEPROM.commit();
+    EEPROM.end();
+    if (status == 0){ Serial.println("\nError Code 1"); while (1); }
+  }
+}
 
 void setup(){
 
 	Serial.setDebugOutput(true);
 	Serial.begin(115200);
+ debuggerInit();
 	Serial.print("Udp server started at port ");
 	Serial.println(CLIENT_PORT);
 	Serial.print("begin =");
@@ -69,12 +92,33 @@ void setup(){
   }
   printWifiStatus();
   serverIP=WiFi.gatewayIP();
-
+TIME=millis();
 }
+void checkConnection(){
+  WiFi.begin(serverSSID, password);
+ int tries=0;
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+    Serial.print(".");
+    tries++;
+    if (tries > 30){
+      break;
+    }
+  }
+  if(WiFi.status() != WL_CONNECTED){unconnected=1;}
+  else{unconnected=0;}
+  printWifiStatus();
+  serverIP=WiFi.gatewayIP();
+  TIME=millis();
+  }
 
 void loop(){
-
-	char inBuffer[32] = {}; //buffer to hold incoming and outgoing packets
+  if(WiFi.status() != WL_CONNECTED){unconnected=1;}
+  if(millis()>TIME+10000 && unconnected){
+    TIME=millis();
+    checkConnection();
+    }
+	char inBuffer[128] = {}; //buffer to hold incoming and outgoing packets
 	String outBuffer = MYNAME;
 	if (Serial.available()){
 		//parse the serial input to send
@@ -91,3 +135,4 @@ void loop(){
 	}
 
 }
+
